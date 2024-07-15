5813c6bb77d499524ce2e01b6c82a492e992e0e0
Rodrigo Miranda <rodrigomiranda.rj@gmail.com>
2024-07-15 17:45:48 -0300

    Update guesscode.py

--- guesscode.py
+++ guesscode.py
@@ -1,9 +1,11 @@
-senha = ['1','2','3','4']
-contrasenha = []
-historico = []
-acerto = False
+import random
 
+senha = [str(random.randrange(0,9)),str(random.randrange(0,9)),str(random.randrange(0,9)),str(random.randrange(0,9))]
+contrasenha = historico = []
+#historico = []
+acerto = False
 while acerto == False:
+    print (f"O código é : {senha}")
     print (f"Histórico: {historico}")
     entrada = input('Digige a senha: ')
     historico.append(entrada)
@@ -13,10 +15,7 @@ while acerto == False:
         if senha[x] == contrasenha[x]:
             pts += 1 
         if pts == 4:
-            print ("Você acertou a senha! Parabéns!!!!")
             acerto = True
-    print (f"Você acertou {pts} números!")
+    msg = f"Você acertou {pts} números!" if pts<4 else "Você acertou o código! Parabéns!!"
+    print (msg)
     print (" ")
-    
-
-
