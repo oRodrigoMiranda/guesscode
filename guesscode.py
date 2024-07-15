0c36b51c9b7a8990fcc298d9583d022ed83bdb0f
Rodrigo Miranda <rodrigomiranda.rj@gmail.com>
2024-07-15 18:05:57 -0300

    Update guesscode.py
    Implementando a escolha da quantidade de dígitos

--- guesscode.py
+++ guesscode.py
@@ -1,21 +1,29 @@
 import random
 
-senha = [str(random.randrange(0,9)),str(random.randrange(0,9)),str(random.randrange(0,9)),str(random.randrange(0,9))]
-contrasenha = historico = []
-#historico = []
+senha = []
+contrasenha = []
+historico = []
 acerto = False
+
+digitos = int(input('Qual será a quantidade de dígitos do código? : '))
+for x in range (digitos): 
+    senha.append (str(random.randrange(0,9)))
+
 while acerto == False:
     print (f"O código é : {senha}")
     print (f"Histórico: {historico}")
-    entrada = input('Digige a senha: ')
+    entrada = input('Digige a sua tentativa: ')
     historico.append(entrada)
+    while len(entrada) != digitos:
+        print('O código possui {digitos} dígitos! tente novamente')
+        entrada = input('Digige a sua tentativa: ')
     contrasenha = list(entrada)
     pts = 0
-    for x in range(len(contrasenha)):
+    for x in range(digitos):
         if senha[x] == contrasenha[x]:
             pts += 1 
-        if pts == 4:
+        if pts == digitos:
             acerto = True
-    msg = f"Você acertou {pts} números!" if pts<4 else "Você acertou o código! Parabéns!!"
+    msg = f"Você acertou {pts} números!" if acerto != True else "Você acertou o código! Parabéns!!"
     print (msg)
     print (" ")
